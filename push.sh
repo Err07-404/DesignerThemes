455b881f68d099176aa81d3ae861c5a1915c3ff2
Err07-404 <zerachyah@gmail.com>
2024-01-22 17:31:46 -0600

    npo11 commit

--- push.sh
+++ push.sh
@@ -2,17 +2,35 @@
 
 STRING="{\"Themes\": {"
 for f in */*; do
+	#f = themes/themename
 	declare -a DOWNLOADS=()
 	STRING=$STRING"\"$(basename $f)\":{"
 	for i in $f/*; do
 		N=$(basename $i)
+		#$N = items in theme folder
 		if [[ $(basename $f).jpg == $N ]] #if ThemeName.jpg = ThemeName.jpg
 		then
-			DOWNLOADS+=($N)
+			DOWNLOADS+=($N) #add images
 			STRING=$STRING"\"img\":\"$N\"," #image
 		elif [[ $N != *.* ]]; #if doesn't contain an extention looking for .
 		then
-			DOWNLOADS+=($N/weathericons)
+			if [[ $N == *signalImages* ]]; #check if contains signalImages
+				then
+					#$i = themes/themeName/signalImages
+					for s in $i/*; do #loop over signalImages/
+						signal=$(basename $s) #get inner folder name
+						DOWNLOADS+=($signal/signalimages) #add to downloads
+					done
+			elif [[ $N == *wifiImages* ]]; #check if contains wifiImages
+				then
+					#$i = themes/themeName/wifiImages
+					for s in $i/*; do #loop over wifiImages/
+						wifi=$(basename $s) #get inner folder name
+						DOWNLOADS+=($wifi/wifiimages) #add to downloads
+					done
+			else 
+				DOWNLOADS+=($N/weathericons)
+			fi
 		else
 			DOWNLOADS+=($N)
 		fi
@@ -37,5 +55,4 @@ git add .
 git commit -m "$*"
 git branch -M main
 git push -u origin main
-
 
